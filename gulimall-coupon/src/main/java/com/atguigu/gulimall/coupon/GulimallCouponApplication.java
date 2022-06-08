54783bffe4503145b47d3a02a2c3a7488c0da2c9
mleelive <74917069+mleelive@users.noreply.github.com>
2022-06-08 21:47:02 +0900

    again commit

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -2,6 +2,7 @@ package com.atguigu.gulimall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class GulimallCouponApplication {
@@ -9,5 +10,5 @@ public class GulimallCouponApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GulimallCouponApplication.class, args);
 	}
-
+	int a = 0;
 }
